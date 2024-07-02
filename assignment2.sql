90f55b684335464a70eb2c5b94b94b707de83129
Shayan bista <shayanbista94@gmail.com>
2024-07-02 23:36:22 +0545

    column add

--- assignment2.sql
+++ assignment2.sql
@@ -10,7 +10,7 @@
     	publisher_id SERIAL PRIMARY KEY,
     	publisher_name VARCHAR(100),
    	 country VARCHAR(50)
-	)
+	);
 
 -- Query to create Books entity in relation to Publisher
 	CREATE TABLE BookStore.Books (
@@ -19,7 +19,7 @@
     	genre VARCHAR(20),
     	publication_year INT,
     	publisher_id INT REFERENCES BookStore.Publisher(publisher_id)
-	)
+	);
 
 -- Query to create Customer entity
    	CREATE TABLE BookStore.Customers (
@@ -27,7 +27,7 @@
     	customer_name VARCHAR(100),
     	email VARCHAR(254),
     	address VARCHAR(100)
-	)
+	);
 
 -- Query to create Orders entity in relation to Customers
     	CREATE TABLE BookStore.Orders (
@@ -35,7 +35,7 @@
     	order_date TIMESTAMP,
     	total_amount INT,
     	customer_id INT REFERENCES BookStore.Customers(customer_id)
-	)
+	);
 
 -- Query to create Author entity
    	CREATE TABLE BookStore.Authors (
@@ -43,18 +43,18 @@
     	author_name VARCHAR(100),
     	birth_date DATE,
     	nationality VARCHAR(50)
-	)
+	);
 
 -- Query to create a many-to-many relation between Orders and Books
 	CREATE TABLE BookStore.Order_Items (
    	order_id INT REFERENCES BookStore.Orders(order_id),
     	book_id INT REFERENCES BookStore.Books(book_id),
     	PRIMARY KEY (order_id, book_id)
-	)
+	);
 
 -- Query to create a many-to-many relation between Authors and Books	
 	CREATE TABLE BookStore.Book_Authors (
     	id INT PRIMARY KEY,
     	book_id INT REFERENCES BookStore.Books(book_id),
     	author_id INT REFERENCES BookStore.Authors(author_id)
-	)
+	);
