a35e3bc501d83c2e8ca764f19a54531128d3f96f
Shayan Bista <84977475+shayanbista@users.noreply.github.com>
2024-07-04 09:54:15 +0545

    Update rank.sql

--- assignment3/rank.sql
+++ assignment3/rank.sql
@@ -1,3 +1,4 @@
+-- Rank students based on the number of courses they are enrolled in, handling ties by assigning the same rank.
 SELECT 
     student_name, 
     RANK() OVER (ORDER BY COUNT(course_id)DESC) AS rank
