35975120aac47cab0ae5a23786130212a449e9a5
Shayan bista <shayanbista94@gmail.com>
2024-07-04 19:03:17 +0545

    all query merge

--- assignment3/rownumber.sql
+++
@@ -1,9 +0,0 @@
---all students along with a row number based on their enrollment date in ascending order.
-SELECT 
-    student_name, 
-    enrollment_date,
-    ROW_NUMBER() OVER (ORDER BY enrollment_date) AS row_num
-FROM 
-    "University".Students
-JOIN 
-    "University".Enrollments ON Students.student_id = Enrollments.student_id;
